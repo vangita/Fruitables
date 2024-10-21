334a44910373bafd6c95a558c7b1f91fee20f687
vangita <vanourjumelashvili@gmail.com>
2024-10-21 17:48:23 +0400

    cart items

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from order.models import UserCart
+from order.models import UserCart, CartItem
 
 admin.site.register(UserCart)
+admin.site.register(CartItem)
--- order/models.py
+++ order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from store.models import Product
 from user.models import User
 
 
@@ -9,3 +10,16 @@ class UserCart(models.Model):
 
       def __str__(self):
             return f"Cart of {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(UserCart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in cart"
+
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
