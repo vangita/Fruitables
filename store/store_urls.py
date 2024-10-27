95142b7c62dbe62edce08be39f4f457e25029e39
vangita <vanourjumelashvili@gmail.com>
2024-10-27 11:00:20 +0400

    FBV to CBV

--- store/store_urls.py
+++ store/store_urls.py
@@ -1,15 +1,20 @@
-from xml.sax import parse
-
 from django.urls import path
-from .views import home, contact_view, category_listing, product_detail, testimonial, get404
+from .views import (
+    HomeView,
+    ContactView,
+    CategoryListingView,
+    ProductDetailView,
+    TestimonialView,
+    Custom404View,
+)
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('product/', product_detail, name='product_detail'),
-  # path('product/<slug:slug>/', product_detail, name='product_detail'),
-    path('contact/',contact_view, name='contact'),
-    path('category/', category_listing, name='generic_category_listing'),
-    path('category/<slug:slug>/', category_listing , name='category_listing'),
-    path('testimonial/', testimonial, name='testimonial'),
-    path('404/', get404 , name='get404'),
+    path('', HomeView.as_view(), name='home'),
+    path('product/', ProductDetailView.as_view(), name='product_detail'),
+    # path('product/<slug:slug>/', ProductDetailView.as_view(), name='product_detail_slug'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('category/', CategoryListingView.as_view(), name='generic_category_listing'),
+    path('category/<slug:slug>/', CategoryListingView.as_view(), name='category_listing'),
+    path('testimonial/', TestimonialView.as_view(), name='testimonial'),
+    path('404/', Custom404View.as_view(), name='get404'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,80 +1,94 @@
-from audioop import error
-
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import TemplateView, ListView, DetailView
+from django.core.paginator import Paginator
 from store.models import Category, Product
-from django.db.models.functions import Cast
 
 
-def home(request):
-    products = Product.objects.all()
-    par_categories = Category.objects.filter(parent__isnull=True)
-    # if slug != None:
-    #     products = products.filter(category__slug=slug)
-    return render(request, 'index.html', {'products': products, 'par_categories': par_categories} )
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        context['par_categories'] = Category.objects.filter(parent__isnull=True)
+        return context
 
 
-
-def contact_view(request):
-    return render(request, 'contact.html')
-
-def category_listing(request, slug=None):
-    tag = request.GET.get('tag', '')
-    price = request.GET.get('rangeInput', '')
-    tags = dict(Product.TAG_CHOICES)
-
-    if slug:
-        parent_category = get_object_or_404(Category, slug=slug)
-        categories = parent_category.subcategoires.all()
-        current_category = get_object_or_404(Category, slug=slug)
-    else:
-        categories = Category.objects.filter(parent__isnull=True)
-        current_category = None
-
-    products = Product.objects.all()
-    if tag :
-        products = products.filter(tag=tag)
-
-    if price!='' and int(price)>0:
-        products = products.filter(price__lte=price)
-
-    sort = request.GET.get('sort', '')
+class ContactView(TemplateView):
+    template_name = 'contact.html'
 
 
-    if sort == 'price_asc':
-        products = products.order_by('price')
-    elif sort == 'price_desc':
-        products = products.order_by('-price')
-    elif sort == 'date_desc':
-        products = products.order_by('-created_at')
-    else:
-        products = products.order_by('name')
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView
+from store.models import Category, Product
 
-    paginator = Paginator(products, 20)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
 
-    par_categories = Category.objects.filter(parent__isnull=True)
-    if slug!=None:
-        page_obj = products.filter(category__slug=slug)
+class CategoryListingView(ListView):
+    model = Product
+    template_name = 'shop.html'
+    context_object_name = 'products'
+    paginate_by = 10
 
-    context = {
-            'categories': categories,
-            'products': page_obj,
-            'par_categories': par_categories,
-            'current_category': current_category,
-            'tags': tags
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        tag = self.request.GET.get('tag', '')
+        price = self.request.GET.get('rangeInput', '')
+        search = self.request.GET.get('q', '')
 
-        }
-    return render(request, 'shop.html', context)
+        if search:
+            queryset = queryset.filter(name__icontains=search)
 
-def product_detail(request):
-    # product = get_object_or_404(Product, slug=slug)
-    return render(request, 'shop-detail.html', )
+        if tag:
+            queryset = queryset.filter(tag=tag)
+        if price and int(price) > 0:
+            queryset = queryset.filter(price__lte=price)
 
-def testimonial(request):
-    return render(request, 'testimonial.html')
+        sort = self.request.GET.get('sort', '')
+        if sort == 'price_asc':
+            queryset = queryset.order_by('price')
+        elif sort == 'price_desc':
+            queryset = queryset.order_by('-price')
+        elif sort == 'date_desc':
+            queryset = queryset.order_by('-created_at')
 
-def get404(request):
-    return render(request, '404.html')
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = queryset.filter(category__slug=slug)
+
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        if 'cart_count' not in request.session:
+            request.session['cart_count'] = 0
+        request.session['cart_count'] += 1
+        request.session.modified = True
+
+        return redirect(request.path)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = self.kwargs.get('slug')
+        if slug:
+            context['parent_category'] = get_object_or_404(Category, slug=slug)
+            context['categories'] = context['parent_category'].subcategoires.all()
+            context['current_category'] = context['parent_category']
+        else:
+            context['categories'] = Category.objects.filter(parent__isnull=True)
+            context['current_category'] = None
+        context['par_categories'] = Category.objects.filter(parent__isnull=True)
+        context['tags'] = dict(Product.TAG_CHOICES)
+
+        context['cart_count'] = self.request.session.get('cart_count', 0)
+        return context
+
+
+class ProductDetailView(TemplateView):
+    template_name = 'shop-detail.html'
+
+
+class TestimonialView(TemplateView):
+    template_name = 'testimonial.html'
+
+
+class Custom404View(TemplateView):
+    template_name = '404.html'
